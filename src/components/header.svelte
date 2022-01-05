662dbf6b6a37d3a8091e29cd3c38163310424586
Raúl <53493295+rguzg@users.noreply.github.com>
2022-01-05 05:53:03 -0600

    feature(MainPage): Add Header

---
+++ src/components/header.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import { Col } from 'sveltestrap';
+</script>
+
+<div class="header">
+    <Col md={2} class="header__logo">
+        <img class="logo" src="/house.svg" alt="House with Laptop"/>
+        <h3>InMob</h3>
+    </Col>
+    <Col md={2} class="header__links">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+    </Col>
+</div>
+
+<style>
+    a{
+        color: #000;
+        text-decoration: none;
+        font-weight: 600;
+    }
+
+    .logo{
+        padding: 5px;
+        height: 50px;
+        width: 50px;
+    }
+
+    .header{
+        justify-content: space-between;
+        display: flex;
+        height: 50px;
+        padding: 5px;
+        width: 100%;
+    }
+
+    /*Due to a limitation with Svelte, component class must be global*/
+    :global(.header__logo){
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    :global(.header__links){
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+</style>
